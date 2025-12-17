b1f64c78cb62c86ab6f396846698919577a3cfb9
TheVy <nvy381932@gmail.com>
2025-12-17 15:03:34 +0700

    push csdl

--- src/main/java/tienich/KetNoiCSDL.java
+++ src/main/java/tienich/KetNoiCSDL.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class KetNoiCSDL {
     // Cấu hình Database
     // Sửa lại đoạn sau dấu gạch chéo cuối cùng thành tên DB thật: smartcityapp
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/smart_city_db";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/smart_city_db.db";
     private static final String USER = "root";
     private static final String PASS = "1234";
 
